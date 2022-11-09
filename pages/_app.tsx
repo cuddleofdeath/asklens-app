0380267ee41dd3a61b7ab0cd7706b4a2f989b7a3
cuddleofdeath <cuddleofdeath@gmail.com>
2022-11-08 18:02:04 -0600

    added NextUI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,23 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { NextUIProvider, createTheme } from '@nextui-org/react';
+
+const darkTheme = createTheme({
+  type: 'dark',
+  theme: {
+    colors: {
+      primaryBg: '#00ebc7',
+      primaryText: '#fff',
+      myColor: 'red',
+      secondary: 'blue',
+    },
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <NextUIProvider theme={darkTheme}>
+      <Component {...pageProps} />
+    </NextUIProvider>
+  );
 }
