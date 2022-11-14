8c006d30ca3ee3ab1c4df0333244f3d9981f914c
cuddleofdeath <cuddleofdeath@gmail.com>
2022-11-14 01:53:13 -0600

    added rainbowkit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,22 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { NextUIProvider, createTheme } from '@nextui-org/react';
-
+import dotenv from 'dotenv';
+import merge from 'lodash.merge';
 import '@rainbow-me/rainbowkit/styles.css';
-import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+  darkTheme as rainbowDarkTheme,
+  midnightTheme,
+  Theme,
+} from '@rainbow-me/rainbowkit';
 import { chain, configureChains, createClient, WagmiConfig } from 'wagmi';
 import { alchemyProvider } from 'wagmi/providers/alchemy';
 import { publicProvider } from 'wagmi/providers/public';
 
 const { chains, provider } = configureChains(
-  [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum],
+  [chain.polygon],
   [alchemyProvider({ apiKey: process.env.ALCHEMY_ID }), publicProvider()]
 );
 
@@ -24,6 +31,36 @@ const wagmiClient = createClient({
   provider,
 });
 
+const theme = createTheme({
+  type: 'dark', // it could be "light" or "dark"
+  theme: {
+    colors: {
+      // brand colors
+      primaryLight: '$green200',
+      primaryLightHover: '$green300',
+      primaryLightActive: '$green400',
+      primaryLightContrast: '$green600',
+      primary: '#4ADE7B',
+      primaryBorder: '$green500',
+      primaryBorderHover: '$green600',
+      primarySolidHover: '$green700',
+      primarySolidContrast: '$white',
+      primaryShadow: '$green500',
+
+      gradient:
+        'linear-gradient(112deg, $blue100 -25%, $pink500 -10%, $purple500 80%)',
+      link: '#5E1DAD',
+
+      // you can also create your own color
+      myColor: '#ff4ecd',
+
+      // ...  more colors
+    },
+    space: {},
+    fonts: {},
+  },
+});
+
 const darkTheme = createTheme({
   type: 'dark',
   theme: {
@@ -32,16 +69,47 @@ const darkTheme = createTheme({
       primaryText: '#fff',
       myColor: 'red',
       secondary: 'blue',
+      connectButtonBackground: 'blue !important',
+      connectButtonBackgroundError: 'red',
+      connectButtonInnerBackground: 'green',
+      connectButtonText: 'yellow',
+      connectButtonTextError: 'orange',
     },
   },
 });
 
+// const myTheme = merge(darkTheme(), {
+//   colors: {
+//     accentColor: '#07296d',
+//   },
+// } as Theme);
+
+// const asklensTheme: Theme =  ({
+//   theme: {
+//     colors: {
+// connectButtonBackground: '...',
+// connectButtonBackgroundError: '...',
+// connectButtonInnerBackground: '...',
+// connectButtonText: '...',
+// connectButtonTextError: '...',
+
+//   });
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <WagmiConfig client={wagmiClient}>
-      <NextUIProvider theme={darkTheme}>
-        <Component {...pageProps} />
-      </NextUIProvider>
+      <RainbowKitProvider
+        theme={rainbowDarkTheme({
+          overlayBlur: 'small',
+          accentColor: '#7b3fe4',
+        })}
+        chains={chains}
+        modalSize='compact'
+      >
+        <NextUIProvider theme={theme}>
+          <Component {...pageProps} />
+        </NextUIProvider>
+      </RainbowKitProvider>
     </WagmiConfig>
   );
 }
