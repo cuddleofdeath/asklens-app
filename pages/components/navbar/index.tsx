b18188bcc1eb4e683baa8431ced1172bfb08b969
cuddleofdeath <cuddleofdeath@gmail.com>
2022-11-14 01:48:21 -0600

    update nav to add rainbow connect

--- pages/components/navbar/index.tsx
+++ pages/components/navbar/index.tsx
@@ -1,4 +1,7 @@
-import { Button, Navbar, Text } from '@nextui-org/react';
+import { Button, Navbar, Text, Link, Card, Radio } from '@nextui-org/react';
+import './navbar.module.css';
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+import ConnectBtn from '../connectButton';
 
 const Nav = ({ text }) => (
   <Navbar
@@ -14,6 +17,31 @@ const Nav = ({ text }) => (
         AskLens
       </Text>{' '}
     </Navbar.Brand>
+    <Navbar.Content
+      hideIn='xs'
+      css={{
+        width: '101vw',
+        justifyContent: 'center',
+      }}
+    >
+      <Navbar.Link href='#'>Features</Navbar.Link>
+      <Navbar.Link isActive href='#'>
+        Customers
+      </Navbar.Link>
+      <Navbar.Link href='#'>Pricing</Navbar.Link>
+      <Navbar.Link href='#'>Company</Navbar.Link>
+    </Navbar.Content>
+    <Navbar.Content
+      css={{
+        marginRight: '2rem',
+      }}
+    >
+      {/* <Button auto flat as={Link} href='#'>
+          Sign Up
+        </Button> */}
+      {/* <ConnectButton label='Sign In' chainStatus='none' showBalance={false} /> */}
+      <ConnectBtn />
+    </Navbar.Content>
   </Navbar>
 );
 
