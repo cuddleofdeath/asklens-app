630125ce0a5e72510e83a704d549a8c36e66891e
cuddleofdeath <cuddleofdeath@gmail.com>
2022-11-13 20:35:25 -0600

    added rainbow, wagmi, ethers

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,28 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { NextUIProvider, createTheme } from '@nextui-org/react';
 
+import '@rainbow-me/rainbowkit/styles.css';
+import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { chain, configureChains, createClient, WagmiConfig } from 'wagmi';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+
+const { chains, provider } = configureChains(
+  [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum],
+  [alchemyProvider({ apiKey: process.env.ALCHEMY_ID }), publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'My RainbowKit App',
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
+
 const darkTheme = createTheme({
   type: 'dark',
   theme: {
@@ -16,8 +38,10 @@ const darkTheme = createTheme({
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <NextUIProvider theme={darkTheme}>
-      <Component {...pageProps} />
-    </NextUIProvider>
+    <WagmiConfig client={wagmiClient}>
+      <NextUIProvider theme={darkTheme}>
+        <Component {...pageProps} />
+      </NextUIProvider>
+    </WagmiConfig>
   );
 }
